4b946bac1249757a75bb70a2f5e4a6043c0a960f
Your Name <you@example.com>
2021-06-09 13:10:15 +0900

    Modified templates to display posts from database.

--- mysite/settingsのコピー.py
+++ mysite/settingsのコピー.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#PROJECT_NAME = os.path.basename(BASE_DIR)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
